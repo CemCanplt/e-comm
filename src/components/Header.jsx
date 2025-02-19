79bd889bc71a9b768709957d0935a7adefbfe29e
CemCanplt <cem--canpolat@hotmail.com>
2025-02-19 17:34:43 +0300

    Mobil Arayüz Fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -246,6 +246,33 @@ function Header() {
               Home
             </Link>
           </li>
+          <li>
+            <Link
+              to="/shop"
+              className="block px-4 py-2 text-(--ikinci-metin-rengi) text-2xl font-semibold hover:bg-gray-100"
+              onClick={toggleMenu}
+            >
+              Shop
+            </Link>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 text-(--ikinci-metin-rengi) text-2xl font-semibold hover:bg-gray-100"
+              onClick={toggleMenu}
+            >
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 text-(--ikinci-metin-rengi) text-2xl font-semibold hover:bg-gray-100"
+              onClick={toggleMenu}
+            >
+              Contact
+            </a>
+          </li>
           {!isAuthenticated && (
             <>
               <li>
